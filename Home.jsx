95ad658ee075681ea6b19702ac0a75d8d6cc4d7b
Lingaramjaipal <lingaramjay@gmail.com>
2025-03-21 09:32:04 +0530

    food_delivary_app_ j

---
+++ Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 align="center">Welcome to the Food Delivary App</h1>
+            <img src="./img" alt="" />
+        </div>
+    )
+}
+
+export default Home
+
